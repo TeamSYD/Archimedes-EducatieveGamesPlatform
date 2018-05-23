c3437d517e5822b11cf578437b3868ad6d82df65
Jotte Smit <jotte_smit@hotmail.com>
2018-05-23 16:29:39 +0200

    footer for homepage/minor fixes.

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule
