a209cdb4527b8291f8dd43aeefc83170eed7f245
Dahir Warsame <dahirwarsame@gmail.com>
2018-05-30 05:29:29 +0200

    Converted: HTML to AngularJS components. Added packages.

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -1,24 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule} from "@angular/forms";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CreateAccountComponent} from './create-account/create-account.component';
+import {ArrangementsComponent} from './arrangements/arrangements.component';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    CreateAccountComponent
+    CreateAccountComponent,
+    ArrangementsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    TooltipModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
