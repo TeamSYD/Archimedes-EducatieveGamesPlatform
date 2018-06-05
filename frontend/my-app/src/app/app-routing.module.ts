9fc554da6c93d4e6ffac9f5049e120eeab9ee1d5
Jotte Smit <jotte_smit@hotmail.com>
2018-06-05 17:38:30 +0200

    grid for card editor + card editor detail start.

--- frontend/my-app/src/app/app-routing.module.ts
+++ frontend/my-app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {CreateAccountComponent} from './create-account/create-account.component';
 import {ArrangementsComponent} from './arrangements/arrangements.component';
 import {CardsComponent} from './cards/cards.component';
+import {CardEditorComponent} from "./card-editor/card-editor.component";
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   {path: 'create-account', component: CreateAccountComponent},
   {path: 'arrangements', component: ArrangementsComponent},
   {path: 'cards', component: CardsComponent},
+  {path: 'card-editor', component: CardEditorComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
 ];
 
