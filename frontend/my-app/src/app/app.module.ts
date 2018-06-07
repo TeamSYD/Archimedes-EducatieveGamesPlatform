3b616a1bdfd1c19e11cca2edcd11feb5e28c591e
Papertray <adminboris@gmail.com>
2018-06-07 03:02:58 +0200

    Fixed the errors and got part of carousel working - ArraySize now correctly assigned TODO: fetch element with and set as item.width TODO: assign dynamic size to outer element

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -1,40 +1,52 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
+import { InMemoryDataService } from './in-memory-data.service';
 
-import {AppComponent} from './app.component';
-import {LoginComponent} from './login/login.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {CreateAccountComponent} from './create-account/create-account.component';
-import {ArrangementsComponent} from './arrangements/arrangements.component';
-import {TooltipModule} from 'ngx-bootstrap/tooltip';
-import {AppRoutingModule} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './footer/footer.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ArrangementsComponent } from './arrangements/arrangements.component';
+
+import { CardsComponent } from './cards/cards.component';
+import { CardSearchComponent } from './card-search/card-search.component';
 import { CardEditorComponent } from './card-editor/card-editor.component';
 import { CardEditorDetailComponent } from './card-editor-detail/card-editor-detail.component';
-import { CardsComponent } from './cards/cards.component';
-import { MessagesComponent }    from './messages/messages.component';
-import { CardSearchComponent } from './card-search/card-search.component';
+import { CarouselComponent, CarouselItemElement } from './carousel/carousel.component';
+import { CarouselItemDirective } from './carousel/carousel-item.directive';
+
+import { MessagesComponent } from './messages/messages.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { CategorySearchComponent } from './category-search/category-search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
+    FooterComponent,
     CreateAccountComponent,
     ArrangementsComponent,
-    FooterComponent,
+
+    CardsComponent,
+    CardSearchComponent,
     CardEditorComponent,
     CardEditorDetailComponent,
-    CardsComponent,
+
+    CarouselItemDirective,
+    CarouselComponent,
+    CarouselItemElement,
+
     MessagesComponent,
-    CardSearchComponent,
     ResourcesComponent,
     CategorySearchComponent
   ],
@@ -42,14 +54,14 @@ import { CategorySearchComponent } from './category-search/category-search.compo
     BrowserModule,
     FormsModule,
     TooltipModule.forRoot(),
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
+      InMemoryDataService, {dataEncapsulation: false}
     )
   ],
   providers: [],
