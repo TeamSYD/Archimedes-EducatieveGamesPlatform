b008c905913a05a14c28f2577f6f131a058fbf51
Jotte Smit <jotte_smit@hotmail.com>
2018-06-15 11:27:44 +0200

    .

--- frontend/my-app/src/app/sets/sets.component.ts
+++ frontend/my-app/src/app/sets/sets.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MatSlideToggle} from "@angular/material";
 
 @Component({
   selector: 'app-sets',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetsComponent implements OnInit {
 
-  constructor() { }
+  isChecked = true;
+
+  constructor() {
+
+  }
+
+  saveButton() {
+
+  }
 
   ngOnInit() {
   }
+
+
 
 }
