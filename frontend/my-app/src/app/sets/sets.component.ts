8ce903dc0810071e7bba4cd13bbbffeef5763210
Boris <adminboris@gmail.com>
2018-06-15 16:34:33 +0200

    Added - Set system

--- frontend/my-app/src/app/sets/sets.component.ts
+++ frontend/my-app/src/app/sets/sets.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sets.component.css']
 })
 export class SetsComponent implements OnInit {
+  setfiller = true;
 
   constructor() { }
 
