2c1002be043ce1031aa58d75c44a2d16ea3d5f1d
Sam <sam.morssinkhof@student.hu.nl>
2018-06-28 00:56:33 +0200

    added maintain game component, changes to gameService

--- src/main/java/com/restservice/archimedes/repository/GameRepository.java
+++ src/main/java/com/restservice/archimedes/repository/GameRepository.java
@@ -1,10 +1,14 @@
 package com.restservice.archimedes.repository;
 
+import com.restservice.archimedes.model.Category;
 import com.restservice.archimedes.model.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+    Page<Game> findByAccountId(long accountId, Pageable pageable);
 
 }
